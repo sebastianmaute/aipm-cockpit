bdb1790766d4c6edde62354d319e25bc2f57449f
Sebastian Maute <65776548+sebastianmaute@users.noreply.github.com>
2026-09-23 23:47:39 +0200

    test: remove the §613 semgrep plant (red proof recorded on PR #10)

--- semgrep-plant/injection-plant.js
+++
@@ -1,8 +0,0 @@
-// Throwaway plant for §613's CI red proof. Removed by the next commit.
-const { exec } = require("child_process");
-
-module.exports = function h(req) {
-  exec(req.query.cmd);
-  eval(req.query.cmd);
-  return new Function(req.query.cmd);
-};
